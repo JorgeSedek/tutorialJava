8fda2de27d22f9ed942a65d35471ce570e8f6852
Jorge <jorgesedek@gmail.com>
2022-05-01 21:10:22 -0300

    pooBasico

---
+++ poo/src/poo/Persona.java
@@ -0,0 +1,32 @@
+package poo;
+
+public class Persona {
+	
+	//atributos
+	String nombre;
+	String apellido;
+	String apodo;
+	int edad;
+	int altura;
+	int peso;
+	
+	//metodos
+	public void comer() {
+		
+	}
+	
+	public void caminar() {
+		
+	}
+	
+	public void correr() {
+		
+	}
+	
+	public void dormir() {
+		
+	}
+	
+	
+	
+}
