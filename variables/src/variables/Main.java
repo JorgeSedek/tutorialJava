c06c16ca9eb3605a275ee48281753ba5135413d7
Jorge <jorgesedek@gmail.com>
2022-05-01 22:58:21 -0300

    variables

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,20 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//tipoDato nombreVar = inicializacion
+		
+		int var = 5;
+		
+		char var2 = 'c';
+		
+		System.out.println("La variable entera var contiene: " + var);
+		
+		System.out.println("La variable entera var2 contiene: " + var2);
+	
+
+	}
+
+}
