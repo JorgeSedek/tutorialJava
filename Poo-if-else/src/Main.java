e03dec22f5be048df389ab43d91065ba26f201e5
Jorge <jorgesedek@gmail.com>
2022-05-01 23:07:56 -0300

    IF-ELSE

---
+++ Poo-if-else/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int num = 10;
+		
+		if(num < 10) {
+			System.out.println("Es mayor a 10");
+		}
+		else {
+			System.out.println("Es igual o menor a 10");
+		}
+
+	}
+
+}
